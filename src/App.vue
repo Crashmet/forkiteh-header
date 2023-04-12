94aa3f375fdfbf83ad8dd7965e277dfa4fe8dc26
Crashmet <shoma.mtv@gmail.com>
2023-04-12 18:48:21 +0300

    add menu btn

--- src/App.vue
+++ src/App.vue
@@ -46,7 +46,10 @@
               class="header-notifications__button"
               id="notifications-search-opener"
             >
-              <div class="header-notifications-button__icon notification-box">
+              <div
+                class="header-notifications-button__icon notification-box"
+                v-if="isMenuActive"
+              >
                 <div class="notification-bell">
                   <span class="bell-top"></span>
                   <span class="notification-count"></span>
@@ -56,40 +59,47 @@
                 </div>
               </div>
             </button>
-            <button class="header-menu__button">
-              <svg
-                width="18"
-                height="18"
-                viewBox="0 0 18 18"
-                fill="none"
-                xmlns="http://www.w3.org/2000/svg"
-              >
-                <path
-                  fill-rule="evenodd"
-                  clip-rule="evenodd"
-                  d="M17.7911 0.209367C17.5142 -0.06751 17.0653 -0.0675106 16.7884 0.209367L9 7.99781L1.21156 0.209367C0.934684 -0.0675106 0.485778 -0.06751 0.208901 0.209367C-0.0679766 0.486244 -0.0679767 0.935151 0.2089 1.21203L7.99734 9.00047L0.2089 16.7889C-0.0679772 17.0658 -0.0679771 17.5147 0.2089 17.7916C0.485777 18.0684 0.934683 18.0684 1.21156 17.7916L9 10.0031L16.7884 17.7916C17.0653 18.0684 17.5142 18.0684 17.7911 17.7916C18.068 17.5147 18.068 17.0658 17.7911 16.7889L10.0027 9.00047L17.7911 1.21203C18.068 0.935151 18.068 0.486244 17.7911 0.209367Z"
-                  fill="white"
-                />
-              </svg>
-              <svg
-                width="18"
-                height="15"
-                viewBox="0 0 18 15"
-                fill="none"
-                xmlns="http://www.w3.org/2000/svg"
-              >
-                <path
-                  fill-rule="evenodd"
-                  clip-rule="evenodd"
-                  d="M0 1.5C0 0.947715 0.447715 0.5 1 0.5H17C17.5523 0.5 18 0.947715 18 1.5C18 2.05228 17.5523 2.5 17 2.5H1C0.447716 2.5 0 2.05228 0 1.5ZM0 7.5C0 6.94772 0.447715 6.5 1 6.5H17C17.5523 6.5 18 6.94772 18 7.5C18 8.05228 17.5523 8.5 17 8.5H1C0.447716 8.5 0 8.05228 0 7.5ZM1 12.5C0.447715 12.5 0 12.9477 0 13.5C0 14.0523 0.447716 14.5 1 14.5H17C17.5523 14.5 18 14.0523 18 13.5C18 12.9477 17.5523 12.5 17 12.5H1Z"
-                  fill="white"
-                />
-              </svg>
+            <button class="header-menu__button" @click="handlerUpMenu">
+              <template v-if="isMenuActive">
+                <svg
+                  width="18"
+                  height="15"
+                  viewBox="0 0 18 15"
+                  fill="none"
+                  xmlns="http://www.w3.org/2000/svg"
+                >
+                  <path
+                    fill-rule="evenodd"
+                    clip-rule="evenodd"
+                    d="M0 1.5C0 0.947715 0.447715 0.5 1 0.5H17C17.5523 0.5 18 0.947715 18 1.5C18 2.05228 17.5523 2.5 17 2.5H1C0.447716 2.5 0 2.05228 0 1.5ZM0 7.5C0 6.94772 0.447715 6.5 1 6.5H17C17.5523 6.5 18 6.94772 18 7.5C18 8.05228 17.5523 8.5 17 8.5H1C0.447716 8.5 0 8.05228 0 7.5ZM1 12.5C0.447715 12.5 0 12.9477 0 13.5C0 14.0523 0.447716 14.5 1 14.5H17C17.5523 14.5 18 14.0523 18 13.5C18 12.9477 17.5523 12.5 17 12.5H1Z"
+                    fill="white"
+                  />
+                </svg>
+              </template>
+              <template v-else>
+                <svg
+                  width="18"
+                  height="18"
+                  viewBox="0 0 18 18"
+                  fill="none"
+                  xmlns="http://www.w3.org/2000/svg"
+                >
+                  <path
+                    fill-rule="evenodd"
+                    clip-rule="evenodd"
+                    d="M17.7911 0.209367C17.5142 -0.06751 17.0653 -0.0675106 16.7884 0.209367L9 7.99781L1.21156 0.209367C0.934684 -0.0675106 0.485778 -0.06751 0.208901 0.209367C-0.0679766 0.486244 -0.0679767 0.935151 0.2089 1.21203L7.99734 9.00047L0.2089 16.7889C-0.0679772 17.0658 -0.0679771 17.5147 0.2089 17.7916C0.485777 18.0684 0.934683 18.0684 1.21156 17.7916L9 10.0031L16.7884 17.7916C17.0653 18.0684 17.5142 18.0684 17.7911 17.7916C18.068 17.5147 18.068 17.0658 17.7911 16.7889L10.0027 9.00047L17.7911 1.21203C18.068 0.935151 18.068 0.486244 17.7911 0.209367Z"
+                    fill="white"
+                  />
+                </svg>
+              </template>
             </button>
           </div>
         </div>
       </section>
-      <section class="header_white">
+      <section
+        class="header__white"
+        :class="isMenuActive ? 'header__white_none' : null"
+      >
         <div class="container-fluid header__line header__line_bottom">
           <nav class="header__nav">
             <ul class="header-nav__list">
@@ -110,7 +120,18 @@
 <script>
 export default {
   name: 'App',
-  components: {},
+  data() {
+    return {
+      isMenuActive: true,
+
+      activNavClass: 'header__nav-item_active',
+    };
+  },
+  methods: {
+    handlerUpMenu() {
+      this.isMenuActive = !this.isMenuActive;
+    },
+  },
 };
 </script>
 
@@ -315,6 +336,8 @@ export default {
   }
 }
 
+/* *** Header Bottom *** */
+
 .header__line_bottom {
   position: relative;
   height: 2.9375rem;
@@ -370,6 +393,10 @@ export default {
 }
 
 @media (max-width: 900px) {
+  .header__white_none {
+    display: none;
+  }
+
   .header__line_top {
     height: 45px;
     margin-bottom: 16px;
